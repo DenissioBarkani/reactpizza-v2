b1d7a59cdc20ac9c3e45a9b863424076f61b1089
Denissio <my.email@example.com>
2025-05-24 15:36:08 +0300

    context start

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,30 @@ import Home from './pages/Home.jsx';
 import NotFound from './pages/NotFound.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Cart from './pages/Cart.jsx';
-import { useState } from 'react';
+import { createContext, useState } from 'react';
+
+const SearchContext = createContext();
 
 function App() {
-    const [searchValue, setSearchValue] = useState('')
+    const [searchValue, setSearchValue] = useState('');
     return (
         <div className="App">
             <div class="wrapper">
-                <Header searchValue={searchValue} setSearchValue={setSearchValue}/>
-                <div class="content">
-                    {/* <div class="container"> */}
-                    <Routes>
-                        <Route path="/" element={<Home searchValue={searchValue}></Home>}></Route>
-                        <Route path="/cart" element={<Cart></Cart>}></Route>
-                        <Route path="*" element={<NotFound></NotFound>}></Route>
-                    </Routes>
+                <SearchContext.Provider value={{searchValue, setSearchValue}}>
+                    <Header />
+                    <div class="content">
+                        {/* <div class="container"> */}
+                        <Routes>
+                            <Route
+                                path="/"
+                                element={<Home searchValue={searchValue}></Home>}></Route>
+                            <Route path="/cart" element={<Cart></Cart>}></Route>
+                            <Route path="*" element={<NotFound></NotFound>}></Route>
+                        </Routes>
 
-                    {/* </div> */}
-                </div>
+                        {/* </div> */}
+                    </div>
+                </SearchContext.Provider>
             </div>
         </div>
     );
